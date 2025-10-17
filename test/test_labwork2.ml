52aa48cd90d374a1b3c5913ee271364a00aef0ec
Temochka <146855997+tteemma@users.noreply.github.com>
2025-10-18 02:58:27 +0300

    build_fix

--- test/test_labwork2.ml
+++ test/test_labwork2.ml
@@ -12,32 +12,27 @@ module D = Labwork2.RBDict.Make(IntOrd)
 
 let suite_unit : test list =
   [
-    "add/find" >:: (fun _ ->
-      let d = D.empty |> D.add 3 "x" |> D.add 1 "a" |> D.add 2 "b" in
-      assert_equal (Some "a") (D.find_opt 1 d);
-      assert_bool "mem 3" (D.mem 3 d);
-      assert_equal 3 (D.size d));
+    "insert & find" >:: (fun _ ->
+      let d = D.empty |> D.add 1 "one" |> D.add 2 "two" in
+      assert_equal (Some "one") (D.find_opt 1 d);
+      assert_equal (Some "two") (D.find_opt 2 d));
 
     "remove" >:: (fun _ ->
       let d = D.empty |> D.add 1 "a" |> D.add 2 "b" |> D.remove 1 in
-      assert_equal None (D.find_opt 1 d);
-      assert_equal (Some "b") (D.find_opt 2 d));
+      assert_bool "key 1 removed" (not (D.mem 1 d));
+      assert_bool "key 2 present" (D.mem 2 d));
 
-    "map/filter" >:: (fun _ ->
-      let d = D.empty |> D.add 1 10 |> D.add 2 20 |> D.map (fun x -> x + 1) in
-      let d' = D.filter (fun k _ -> k = 2) d in
-      assert_equal (Some 11) (D.find_opt 1 d);
-      assert_equal None (D.find_opt 1 d');
-      assert_equal (Some 21) (D.find_opt 2 d'));
+    "map & filter" >:: (fun _ ->
+      let d = D.empty |> D.add 1 10 |> D.add 2 20 in
+      let d' = D.map (fun x -> x + 1) d in
+      let d'' = D.filter (fun k _ -> k = 1) d' in
+      assert_equal (Some 11) (D.find_opt 1 d'');
+      assert_equal None (D.find_opt 2 d''));
 
-    "monoid identity" >:: (fun _ ->
-      let a = D.empty |> D.add 1 "a" in
-      let b = D.empty |> D.add 2 "b" in
-      let u = D.union a b in
-      assert_bool "contains 1" (D.mem 1 u);
-      assert_bool "contains 2" (D.mem 2 u);
-      assert_bool "left identity" (D.equal String.equal a (D.union D.empty a));
-      assert_bool "right identity" (D.equal String.equal a (D.union a D.empty)));
+    "folds" >:: (fun _ ->
+      let d = D.empty |> D.add 1 1 |> D.add 2 2 |> D.add 3 3 in
+      let sum = D.fold_left (fun acc _ v -> acc + v) 0 d in
+      assert_equal 6 sum);
   ]
 
 (* ---------- PROPERTY TESTS ---------- *)
@@ -47,36 +42,49 @@ let arb_assoc_list = QCheck.(list gen_pair)
 
 let as_dict lst = List.fold_left (fun d (k,v) -> D.add k v d) D.empty lst
 
-let prop_union_assoc =
-  Test.make ~name:"union associative"
+let prop_left_id =
+  Test.make ~name:"append left identity"
+    arb_assoc_list
+    (fun a ->
+      let da = as_dict a in
+      D.equal String.equal (D.union D.empty da) da)
+
+let prop_right_id =
+  Test.make ~name:"append right identity"
+    arb_assoc_list
+    (fun a ->
+      let da = as_dict a in
+      D.equal String.equal (D.union da D.empty) da)
+
+let prop_assoc =
+  Test.make ~name:"append associativity"
     QCheck.(triple arb_assoc_list arb_assoc_list arb_assoc_list)
     (fun (a,b,c) ->
-      let da,db,dc = as_dict a, as_dict b, as_dict c in
+      let da, db, dc = as_dict a, as_dict b, as_dict c in
       D.equal String.equal
         (D.union (D.union da db) dc)
         (D.union da (D.union db dc)))
 
-let prop_empty_identity =
-  Test.make ~name:"empty identity"
-    arb_assoc_list
-    (fun a ->
-      let d = as_dict a in
-      D.equal String.equal d (D.union d D.empty)
-      && D.equal String.equal d (D.union D.empty d))
+let prop_map_comp =
+  Test.make ~name:"map composition"
+    QCheck.(list (pair small_nat small_nat))
+    (fun lst ->
+      let d = as_dict lst in
+      let f x = x + 1 and g x = x * 2 in
+      let left = D.map (fun x -> f (g x)) d
+      and right = D.map f (D.map g d) in
+      D.equal (=) left right)
 
-let prop_lookup_after_add =
-  Test.make ~name:"find after add"
-    gen_pair
-    (fun (k,v) ->
-      match D.find_opt k (D.add k v D.empty) with
-      | Some v' -> String.equal v v'
-      | None -> false)
+let suite_props : test list =
+  [
+    QCheck_ounit.to_ounit2_test prop_left_id;
+    QCheck_ounit.to_ounit2_test prop_right_id;
+    QCheck_ounit.to_ounit2_test prop_assoc;
+    QCheck_ounit.to_ounit2_test prop_map_comp;
+  ]
 
-(* ---------- MAIN ---------- *)
+(* ---------- MAIN RUNNER ---------- *)
 
 let () =
-  let q : test list =
-    [ prop_union_assoc; prop_empty_identity; prop_lookup_after_add ]
-    |> List.map QCheck_ounit.to_ounit2_test
-  in
-  run_test_tt_main ("all" >::: (suite_unit @ q))
+  run_test_tt_main
+    ("Labwork2 Dict" >::: (suite_unit @ suite_props))
